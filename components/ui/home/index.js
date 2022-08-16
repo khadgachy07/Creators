8b1c3faa6b4c9eb1bd33de76c0157ecbc3c90eca
khadgachy07 <khadgachy07@gmail.com>
2022-08-17 00:18:01 +0545

    Components setup

---
+++ components/ui/home/index.js
@@ -0,0 +1,4 @@
+
+
+export { default as AddCreators } from './add';
+export { default as RemoveCreators } from './remove';
---
+++ components/ui/person/index.js
@@ -0,0 +1,5 @@
+
+
+export {default as Name} from './name';
+export {default as Hash} from './hash';
+export {default as PersonHash} from './personhash';
