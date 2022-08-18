5965c602002165c7352d28362a25232ce1c2a57f
khadgachy07 <khadgachy07@gmail.com>
2022-08-19 01:19:58 +0545

    Contract Loaded using web3

--- components/providers/web3/index.js
+++ components/providers/web3/index.js
@@ -19,7 +19,7 @@ export default function Web3Provider({children}){
             const provider = await detectEthereumProvider()
             if(provider){
                 const web3 = new Web3(provider)
-                const contract = await loadContract("StarterContract", provider)
+                const contract = await loadContract("StarterContract", web3)
                 console.log(contract)
                 setWeb3Api({
                     provider,
--- utils/loadContract.js
+++ utils/loadContract.js
@@ -1,21 +1,22 @@
-import contract from "@truffle/contract"
-import Contract from "@truffle/contract/lib/contract"
 
+const NETWORK_ID = process.env.NEXT_PUBLIC_NETWORK_ID
 
-export const loadContract = async (name, provider) => {
+export const loadContract = async (name, web3) => {
   const res = await fetch(`/contracts/${name}.json`)
   const Artifact = await res.json()
-
-  const _contract = contract(Artifact)
   
-  _contract.setProvider(provider)
+  let contract = null 
 
-  let deployedContract = null
-
-  try {
-    deployedContract = await _contract.deployed()
-  } catch {
+  try{
+    contract = new web3.eth.Contract(
+      Artifact.abi,
+      Artifact.networks[NETWORK_ID].address
+    )
+    console.log(contract)
+  }catch{
     console.log(`Contract ${name} cannot be loaded`)
   }
-  return deployedContract
+  return contract
 }
+
+
