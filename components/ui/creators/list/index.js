c3de9751bea1ad84851b210f7b43eb5c849f9aae
khadgachy07 <khadgachy07@gmail.com>
2022-08-19 20:01:47 +0545

    GetCreator Handled

--- components/ui/creators/list/index.js
+++ components/ui/creators/list/index.js
@@ -1,7 +1,28 @@
+import { useWeb3 } from "@components/providers";
 import { Button } from "@components/ui/common";
 import Card from "@components/ui/common/cards";
+import { useAccount } from "@components/web3/hooks/useAccount";
 
 export default function List() {
+  const { account } = useAccount();
+  const { contract } = useWeb3();
+
+  const handleGetCreator = async() => {
+    try {
+      if (account.data && contract) {
+        const result = await contract.methods
+          .getCretors()
+          .call({ from: account.data });
+        console.log(result);
+        alert('Successfully got creators ')
+      } else {
+        alert("Please Connect to metamask");
+      }
+    } catch (error) {
+      alert("Function failed");
+    }
+  };
+
   return (
     <div>
       <div>
@@ -10,10 +31,12 @@ export default function List() {
         </h3>
       </div>
       <div>
-            <Button>Get Creator</Button>
+            <Button onClick={handleGetCreator}>Get Creator</Button>
         </div>
         <div className= "flex flex-wrap -mx-3 my-6">
-            <Card/>
+            <ul>
+              <li>result[1].age</li>
+            </ul>
         </div>
     </div>
   );
